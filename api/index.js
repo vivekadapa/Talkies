840c9b8b4cd1565834bf382ae426ca5fa9289dfe
vivekchaitanya <vivekchaitanya1324@gmail.com>
2024-07-12 00:31:43 +0530

    cors error deployment

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ const server = process.env.server;
 dbConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://talkies-frontend.onrender.com/' }));
 
 
 app.use('/auth', authRouter);
